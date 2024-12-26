5f3cd5cd6ed919494a708484373dc4211a3b45dc
hozl03 <hozl-wp21@student.tarc.edu.my>
2024-12-27 01:45:23 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -396,4 +396,4 @@ if st.button('Predict'):
 
         # elif model_choice == 'Random Forest':
     random_forest_pred = loaded_random_forest.predict(X)
-    st.write(f"**Random Forest Prediction: ${random_forest_pred[0]:,.2f}**")
+    st.write(f"**Random Forest Prediction: {random_forest_pred[0]:}**")
