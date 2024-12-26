6188c64bd000f6b5e66db0d1035abd8d3ec05894
hozl03 <hozl-wp21@student.tarc.edu.my>
2024-12-26 23:37:21 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,7 +13,7 @@ from sklearn.model_selection import train_test_split, cross_val_score
 from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import r2_score, mean_absolute_error, mean_squared_error
 
-import sklearn # import the module so you can use it
+import sklearn
 from sklearn.linear_model import LinearRegression
 from sklearn.linear_model import Ridge
 from sklearn.linear_model import Lasso
