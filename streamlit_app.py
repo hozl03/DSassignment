b385c89608ffe603a5c64bdecce48facefc34fb0
hozl03 <hozl-wp21@student.tarc.edu.my>
2024-12-27 20:08:03 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,11 +40,11 @@ from xgboost import XGBRegressor
 from sklearn.preprocessing import PolynomialFeatures
 
 import joblib
-# from tensorflow.keras.models import load_model
+from tensorflow.keras.models import load_model
 
 loaded_random_forest = joblib.load('random_forest_model.joblib')
 loaded_catboost = joblib.load('catboost_model.joblib')
-# loaded_nn = load_model('neural_network_model.h5')
+loaded_nn = load_model('neural_network_model.h5')
 
 
 
