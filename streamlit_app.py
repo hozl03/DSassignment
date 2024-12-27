c6a8313f4b70d395b2151705f5849682abe5ae85
hozl03 <hozl-wp21@student.tarc.edu.my>
2024-12-27 19:57:15 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -395,22 +395,34 @@ st.write("## Prediction Results")
 model_choice = st.selectbox('Select Model', ['Random Forest Classifier', 'Support Vector Classifier', 'Neural Network', 'Category Boost'])
 
 if st.button('Predict'):
+    if model_choice == 'Neural Network':
+        nn_pred = loaded_nn.predict(X)
+        st.write(f"**Neural Network Prediction: {nn_pred[0]:,.2f}**")
+
+    elif model_choice == 'Category Boost':
+        catboost_pred = loaded_catboost.predict(X)
+        st.write(f"**Category Boost Prediction: {catboost_pred[0]:,.2f}**")
+
+    elif model_choice == 'Random Forest':
+        random_forest_pred = loaded_random_forest.predict(X)
+        st.write(f"**Random Forest Prediction: {random_forest_pred[0]:}**")
+
     # Check for missing inputs (NaN or None values)
     # missing_values = isnull().sum()
 
     # Check if there are any missing values in the user's input
-    if missing_values.any():
-        st.error(f"Please fill out all the required fields. Missing values: {list(input_df.columns[missing_values > 0])}")
-    else:
+    # if missing_values.any():
+    #     st.error(f"Please fill out all the required fields. Missing values: {list(input_df.columns[missing_values > 0])}")
+    # else:
         # Proceed with prediction only if no values are missing
-        if model_choice == 'Neural Network':
-            nn_pred = loaded_nn.predict(X)
-            st.write(f"**Neural Network Prediction: {nn_pred[0]:,.2f}**")
+        # if model_choice == 'Neural Network':
+        #     nn_pred = loaded_nn.predict(X)
+        #     st.write(f"**Neural Network Prediction: {nn_pred[0]:,.2f}**")
 
-        elif model_choice == 'Category Boost':
-            catboost_pred = loaded_catboost.predict(X)
-            st.write(f"**Category Boost Prediction: {catboost_pred[0]:,.2f}**")
+        # elif model_choice == 'Category Boost':
+        #     catboost_pred = loaded_catboost.predict(X)
+        #     st.write(f"**Category Boost Prediction: {catboost_pred[0]:,.2f}**")
 
-        elif model_choice == 'Random Forest':
-            random_forest_pred = loaded_random_forest.predict(X)
-            st.write(f"**Random Forest Prediction: {random_forest_pred[0]:}**")
+        # elif model_choice == 'Random Forest':
+        #     random_forest_pred = loaded_random_forest.predict(X)
+        #     st.write(f"**Random Forest Prediction: {random_forest_pred[0]:}**")
